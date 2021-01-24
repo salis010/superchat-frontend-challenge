ba9697f1961f14ab4f0f2b5647e056d6c58b433b
Stephen <stephen.saliba@gmail.com>
2021-01-24 12:07:17 +0100

    Tweaks Star Button

---
+++ src/components/card/star-button.tsx
@@ -0,0 +1,45 @@
+import * as React from "react"
+import styled from "../../styles/styled"
+import { Button } from "./button"
+import { WIDTHS, HEIGHTS, RADII, FONT_SIZES } from "./consts"
+
+const ButtonWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 3rem 3rem 3rem;
+    background-color: ${(props) => props.backgroundColor};
+    border-radius: 0 0 3rem 3rem;
+`
+
+interface IStarButton {
+    color: string
+    backgroundColor: string
+    buttonSize: string
+}
+
+export const StarButton = ({
+    color,
+    backgroundColor,
+    buttonSize,
+}: IStarButton) => {
+    const fontSize = FONT_SIZES[buttonSize]
+    const width = WIDTHS[buttonSize]
+    const height = HEIGHTS[buttonSize]
+    const radius = RADII[buttonSize]
+
+    return (
+        <ButtonWrapper backgroundColor={backgroundColor}>
+            <Button
+                color={color}
+                backgroundColor={backgroundColor}
+                fontSize={fontSize}
+                width={width}
+                height={height}
+                radius={radius}
+            >
+                Star
+            </Button>
+        </ButtonWrapper>
+    )
+}

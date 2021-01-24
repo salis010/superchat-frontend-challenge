85436deddad53444fc3b03f3da62825139420e6a
Stephen <stephen.saliba@gmail.com>
2021-01-24 15:45:31 +0100

    Creates function to create link

---
+++ src/components/link-creator/get-link.ts
@@ -0,0 +1,5 @@
+export const getLink = (origin, linkParams) => {
+    const queryParams = Object.entries(linkParams).map((param) => `${param[0]}=${param[1]}`).join('&')
+    
+    return `${origin}?` + queryParams
+}
